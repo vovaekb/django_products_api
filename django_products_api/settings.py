6b8c7bf0b3a00c5ddfc31c1e0bc6a622a45e3b54
Vladimir Privalov <vovaprivalov@gmail.com>
2023-03-23 11:44:43 +0400

    Remove secret

--- django_products_api/settings.py
+++ django_products_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a751_0a9joyxxn!$w=m&7%c6i1^$*mjhx!)=#n^%j3@m7r(tk6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
